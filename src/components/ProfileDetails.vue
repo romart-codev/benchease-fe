e8204f1e5411413f8f8681f3b553ca5858dcc9b3
romart-codev <romarta@codev.com>
2023-04-19 13:25:56 +0800

    Initial setup for pages

---
+++ src/components/ProfileDetails.vue
@@ -0,0 +1,77 @@
+<template>
+  <section class="card shadow">
+    <img class="card-img" src="../assets/user.png" alt="user" />
+    <div class="theme-bg-color card-header text-white">
+      <h1>John Doe</h1>
+      <p>UI/UX Designer</p>
+    </div>
+    <div class="card-body">
+      <div class="container">
+        <div class="row align-items-start">
+          <div class="col">
+            <img src="../assets/location.png" alt="user" />
+            <span>Hybrid</span>
+          </div>
+          <div class="col">
+            <img src="../assets/time.png" alt="user" />
+            <span>7:00AM - 4:00PM </span>
+          </div>
+          <div class="col">
+            <img src="../assets/mail.png" alt="user" />
+            <span>johnd@codev.com</span>
+          </div>
+        </div>
+        <div class="row align-items-center">
+          <div class="col">
+            <img src="../assets/people.png" alt="user" />
+            <span>Codev Talent Mgmt.</span>
+          </div>
+          <div class="col">
+            <img src="../assets/calendar.png" alt="user" />
+            <span>Tuesday - Saturday</span>
+          </div>
+          <div class="col">
+            <img src="../assets/phone.png" alt="user" />
+            <span>09123456789</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "ProfileDetails",
+});
+</script>
+<style lang="scss">
+.card-img {
+  position: absolute;
+  left: 90px;
+  z-index: 1;
+  top: 60px;
+  max-width: 157px;
+}
+.card-header {
+  padding-left: 300px !important;
+  padding-top: 54px !important;
+  padding-bottom: 4px !important;
+  height: 136px;
+  text-align: left;
+}
+.card-body {
+  padding-left: 320px !important;
+  margin: 31px 0;
+  text-align: left;
+
+  .col {
+    margin-bottom: 21px;
+  }
+
+  img {
+    padding-right: 15px;
+  }
+}
+</style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,71 @@
+<template>
+  <aside class="col-auto col-md-3 col-xl-2 px-sm-2 px-0 theme-bg-color">
+    <div
+      class="d-flex flex-column align-items-center align-items-sm-start w-100 px-3 pt-2 text-white min-vh-100"
+    >
+      <a
+        href="/"
+        class="d-flex align-items-center pb-3 mx-auto text-decoration-none"
+      >
+        <span class="fs-5 d-none d-sm-inline text-white">
+          <img alt="Vue logo" src="../assets/logo.png" />
+        </span>
+      </a>
+      <ul
+        class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start w-100"
+        id="menu"
+      >
+        <li class="nav-item">
+          <a href="#" class="nav-link align-middle px-0">
+            <i class="fs-4 bi-house"></i>
+            <span class="ms-1 d-none d-sm-inline text-white">Dashboard</span>
+          </a>
+        </li>
+        <li>
+          <a
+            href="#submenu1"
+            data-bs-toggle="collapse"
+            class="nav-link px-0 align-middle"
+          >
+            <i class="fs-4 bi-speedometer2"></i>
+            <span class="ms-1 d-none d-sm-inline text-white">Skills</span>
+          </a>
+          <!-- <ul
+            class="collapse show nav flex-column ms-1"
+            id="submenu1"
+            data-bs-parent="#menu"
+          >
+            <li class="w-100">
+              <a href="#" class="nav-link px-0">
+                <span class="d-none d-sm-inline">Item</span> 1
+              </a>
+            </li>
+            <li>
+              <a href="#" class="nav-link px-0">
+                <span class="d-none d-sm-inline">Item</span> 2
+              </a>
+            </li>
+          </ul> -->
+        </li>
+        <li>
+          <a href="#" class="nav-link px-0 align-middle">
+            <i class="fs-4 bi-table"></i>
+            <span class="ms-1 d-none d-sm-inline text-white">Settings</span></a
+          >
+        </li>
+        <li>
+          <a
+            href="#submenu2"
+            data-bs-toggle="collapse"
+            class="nav-link px-0 align-middle"
+          >
+            <i class="fs-4 bi-bootstrap"></i>
+            <span class="ms-1 d-none d-sm-inline text-white">Logout</span></a
+          >
+        </li>
+      </ul>
+      <hr />
+      <div>Bench Ease © {{ new Date().getFullYear() }}</div>
+    </div>
+  </aside>
+</template>
